10bdc7da864fb03852b2164918c5c884856dee9f
celinegodichon <celine.godichon@hotmail.fr>
2020-05-27 19:25:51 +0200

    style on login and register page

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,71 +4,94 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr'  => [
+                    'class' => 'form-control mb-2'
+                ]
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'label' => 'Mot de passe',
+                'attr'  => [
+                    'class' => 'form-control mb-2'
+                ],
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Merci de rentrer un mot de passe',
                     ]),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        'min'           => 6,
+                        'minMessage'    => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'max'           => 4096,
                     ]),
                 ],
             ])
             ->add('firstname', TextType::class, [
                 'label' => 'Prénom',
+                'attr'  => [
+                    'class' => 'form-control mb-2'
+                ]
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Nom',
+                'attr'  => [
+                    'class' => 'form-control mb-2'
+                ]
             ])
             ->add('startDate', DateType::class, [
-                'format' => 'dd-MM-yyyy',
-                'label' => 'Date de début de régime',
-                'required' => false,
-                'placeholder' => [
-                    'year' => 'Année', 'month' => 'Mois', 'day' => 'Jour',
+                'widget'    => 'single_text',
+                'label'     => 'Date de début de régime ',
+                'required'  => false,
+                'attr'      => [
+                    'class' => ' form-control mb-2'
                 ]
             ])
             ->add('age', TextType::class, [
                 'label' => 'Age',
-                'required' => false,
+                'attr'  => [
+                    'class' => 'form-control mb-2'
+                ],
+                'required'  => false,
             ])
             ->add('weight', TextType::class, [
                 'label' => 'Poids',
+                'attr'  => [
+                    'class' => 'form-control mb-2'
+                ],
                 'required' => false,
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'label' => 'Accepter nos conditions',
+                'attr'  => [
+                    'class' => 'form-control mb-2'
+                ],
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'Merci d\'accepter nos conditions',
                     ]),
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
