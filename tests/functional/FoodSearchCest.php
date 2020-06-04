d5c09e131ebb9c679ce6b66086983f9ff8c957c8
anne.quiedeville <anne.quiedeville@dotsafe.fr>
2020-06-04 15:08:41 +0200

    added test on food search

---
+++ tests/functional/FoodSearchCest.php
@@ -0,0 +1,30 @@
+<?php namespace App\Tests;
+use App\Tests\FunctionalTester;
+
+class FoodSearchCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function FoodSearch(FunctionalTester $I)
+    {
+        // Sign in
+        $I->amOnPage('/');
+        $I->fillField('email','testy@test.fr');
+        $I->fillField('password','coucou');
+        $I->click('Connexion');
+
+        // Go to food section and search by name
+        $I->see('Aliments');
+        $I->click('Aliments');
+        $I->see('Bacon');
+        $I->see('Clementine');
+        $I->see('Chercher');
+        $I->fillField('food_search[searchText]', 'clementine');
+        $I->click('Chercher');
+        $I->dontSee('Bacon');
+        $I->see('Clementine');
+    }
+}
