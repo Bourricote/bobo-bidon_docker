e7558cff7207b021eb7d9c3fbf6112e2a43a7359
anne.quiedeville <anne.quiedeville@dotsafe.fr>
2020-06-04 16:09:57 +0200

    updated forms category and symptom

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,18 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('dietWeek')
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr'  => [
+                    'class' => 'form-control mb-2'
+                ]
+            ])
+            ->add('dietWeek',  NumberType::class, [
+                'label' => 'Semaine de régime',
+                'attr'  => [
+                    'class' => 'form-control mb-2'
+                ]
+            ])
         ;
     }
 
