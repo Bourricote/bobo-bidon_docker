2786e6fe4da0215490fc185cf88c9082f4e5557d
anne.quiedeville <anne.quiedeville@orange.fr>
2020-06-19 12:00:01 +0200

    added test cases for ChartServiceTest

--- tests/unit/ChartServiceTest.php
+++ tests/unit/ChartServiceTest.php
@@ -9,49 +9,61 @@ use App\Service\ChartService;
 use App\Service\UserService;
 use DateInterval;
 use DateTime;
+use DateTimeImmutable;
 use Doctrine\Common\Persistence\ManagerRegistry;
 
 class ChartServiceTest extends \Codeception\Test\Unit
 {
     /**
-     * @var \App\Tests\UnitTester
+     * @var UnitTester
      */
     protected $tester;
-    private $user;
+    private $userDietInProgress;
+    private $userDietNotStarted;
+    private $userDietEnded;
     private $symptom;
     private $categories;
     private $chartService;
     
     protected function _before()
     {
-        // Create user with start and end dates
-        $this->user = new User();
-        $this->user->setStartDate(new DateTime());
-        $startDate = clone $this->user->getStartDate();
-        $this->user->setEndDate($startDate->add(new DateInterval('P56D')));
+        // Create userDietInProgress with start and end dates
+        $this->userDietInProgress = new User();
+        $this->userDietInProgress->setStartDate(new DateTime());
+        $startDate = clone $this->userDietInProgress->getStartDate();
+        $this->userDietInProgress->setEndDate($startDate->add(new DateInterval('P56D')));
 
-        // Create one symptom and add it twice on same day for user and 2 weeks after
+        // Create userDietNotStarted with no dates
+        $this->userDietNotStarted = new User();
+
+        // Create userDietEnded with end date in the past
+        $this->userDietEnded = new User();
+        $today = new DateTimeImmutable();
+        $this->userDietEnded->setStartDate($today->sub(new DateInterval('P58D')));
+        $this->userDietEnded->setEndDate($today->sub(new DateInterval('P2D')));
+
+        // Create one symptom and add it twice on same day for userDietInProgress and 2 weeks after
         $this->symptom = new Symptom();
         $this->symptom->setName('MalPartout');
 
         $userSymptom = new UserSymptom();
-        $userSymptom->setSymptom($this->symptom)->setUser($this->user);
+        $userSymptom->setSymptom($this->symptom)->setUser($this->userDietInProgress);
         $userSymptom->setDate(new DateTime());
 
         $userSymptom2 = new UserSymptom();
-        $userSymptom2->setSymptom($this->symptom)->setUser($this->user);
+        $userSymptom2->setSymptom($this->symptom)->setUser($this->userDietInProgress);
         $userSymptom2->setDate(new DateTime());
 
         $userSymptom3 = new UserSymptom();
-        $userSymptom3->setSymptom($this->symptom)->setUser($this->user);
+        $userSymptom3->setSymptom($this->symptom)->setUser($this->userDietInProgress);
         $userSymptom3->setDate((new DateTime())->add(new DateInterval('P16D')));
 
-        $this->user->addUserSymptom($userSymptom);
-        $this->user->addUserSymptom($userSymptom2);
-        $this->user->addUserSymptom($userSymptom3);
+        $this->userDietInProgress->addUserSymptom($userSymptom);
+        $this->userDietInProgress->addUserSymptom($userSymptom2);
+        $this->userDietInProgress->addUserSymptom($userSymptom3);
 
         // Check number of symptoms for user
-        $userSymptoms = $this->user->getUserSymptoms();
+        $userSymptoms = $this->userDietInProgress->getUserSymptoms();
         $this->assertCount(3, $userSymptoms);
 
         // Create categories and put them in an array
@@ -74,38 +86,73 @@ class ChartServiceTest extends \Codeception\Test\Unit
     // tests
     public function testGenerateDataPerDay()
     {
-        $dataDaysChart = $this->chartService->generateDataPerDay($this->user);
-        $this->assertEquals(date_format(new DateTime(), 'd/m/Y'), $dataDaysChart['labelDays'][0]);
-        $this->assertEquals(2, $dataDaysChart['nbSymptomsPerDay'][0]);
+        // test for userDietInProgress
+        $data = $this->chartService->generateDataPerDay($this->userDietInProgress);
+        $this->assertEquals(date_format(new DateTime(), 'd/m/Y'), $data['labelDays'][0]);
+        $this->assertEquals(2, $data['nbSymptomsPerDay'][0]);
+
+        // test for userDietNotStarted
+        $data = $this->chartService->generateDataPerDay($this->userDietNotStarted);
+        $this->assertEquals(null, $data);
     }
 
     public function testGenerateDataPerWeek()
     {
-        $dataWeeksChart = $this->chartService->generateDataPerWeek($this->user, $this->categories);
-        $this->assertEquals('3. Les Trucs 3', $dataWeeksChart['labelWeeks'][2]);
-        $this->assertEquals(2, $dataWeeksChart['nbSymptomsPerWeek'][0]);
+        // test for userDietInProgress
+        $data = $this->chartService->generateDataPerWeek($this->userDietInProgress, $this->categories);
+        $this->assertEquals('3. Les Trucs 3', $data['labelWeeks'][2]);
+        $this->assertEquals(2, $data['nbSymptomsPerWeek'][0]);
+
+        // test for userDietNotStarted
+        $data = $this->chartService->generateDataPerWeek($this->userDietNotStarted, $this->categories);
+        $this->assertEquals(null, $data);
     }
 
     public function testGenerateDataPerWeekPerSymptom()
     {
-        $data = $this->chartService->generateDataPerWeekPerSymptom($this->user, $this->symptom);
+        // test for userDietInProgress
+        $data = $this->chartService->generateDataPerWeekPerSymptom($this->userDietInProgress, $this->symptom);
         $this->assertEquals(2, $data['nbSymptomsPerWeek'][0]);
+
+        // test for userDietNotStarted
+        $data = $this->chartService->generateDataPerWeekPerSymptom($this->userDietNotStarted, $this->symptom);
+        $this->assertEquals(null, $data);
     }
 
     public function testGenerateDataForDietWeeks()
     {
-        $data = $this->chartService->generateDataForDietWeeks($this->user, $this->categories);
-        $this->assertEquals(0, $data['weeks_data'][0]);
-        $this->assertEquals(56, $data['weeks_data'][1]);
-        $this->assertEquals('Vous êtes à la semaine 1 de votre régime !', $data['message']);
-        $this->assertEquals(null, $data['category']);
+        // test for userDietInProgress
+        $data1 = $this->chartService->generateDataForDietWeeks($this->userDietInProgress, $this->categories);
+        $this->assertEquals(0, $data1['weeks_data'][0]);
+        $this->assertEquals(56, $data1['weeks_data'][1]);
+        $this->assertEquals('Vous êtes à la semaine 1 de votre régime !', $data1['message']);
+        $this->assertEquals(null, $data1['category']);
+
+        // test for userDietNotStarted
+        $data2 = $this->chartService->generateDataForDietWeeks($this->userDietNotStarted, $this->categories);
+        $this->assertEquals(0, $data2['weeks_data'][0]);
+        $this->assertEquals(56, $data2['weeks_data'][1]);
+        $this->assertEquals('Vous n\'avez pas commencé votre régime !', $data2['message']);
+        $this->assertEquals(null, $data2['category']);
+
+        // test for userDietEnded
+        $data3 = $this->chartService->generateDataForDietWeeks($this->userDietEnded, $this->categories);
+        $this->assertEquals(56, $data3['weeks_data'][0]);
+        $this->assertEquals(0, $data3['weeks_data'][1]);
+        $this->assertEquals('Vous avez fini votre régime !', $data3['message']);
+        $this->assertEquals(null, $data3['category']);
     }
 
     public function testGenerateDataForCategories()
     {
-        $data = $this->chartService->generateDataForCategories($this->user, $this->categories);
+        // test for userDietInProgress
+        $data = $this->chartService->generateDataForCategories($this->userDietInProgress, $this->categories);
         $this->assertEquals('Les Trucs 3', $data['categories_labels'][0]);
         $this->assertEquals(1, $data['nbSymptomsPerWeek'][0]);
         $this->assertEquals('Les Trucs 3', $data['worst_category']->getName());
+
+        // test for userDietNotStarted
+        $data = $this->chartService->generateDataForCategories($this->userDietNotStarted, $this->categories);
+        $this->assertEquals(null, $data);
     }
 }
