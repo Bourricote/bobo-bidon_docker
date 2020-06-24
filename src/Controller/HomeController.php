05ef2d29a89d61d548d1cd8729f452c6828b8850
anne.quiedeville <anne.quiedeville@orange.fr>
2020-06-24 14:57:17 +0200

    renamed Deafult Controller to Home Controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends AbstractController
+class HomeController extends AbstractController
 {
     /**
      * @Route("/home/{user}", name="home")
